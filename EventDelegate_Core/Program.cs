5b865953132ba380805540a13d59e04bceff17d3
Frank <franklanglois@gmail.com>
2017-09-29 09:07:29 -0400

    Add project files.

---
+++ EventDelegate_Core/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EventDelegate_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
